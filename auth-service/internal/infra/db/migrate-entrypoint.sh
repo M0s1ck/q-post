471030044c50f32dacb93caded8e89c27d67266a
M0s1ck <senyamoskin@gmail.com>
2025-11-17 13:54:15 +0300

    integrate new migrating to ci-cd

--- auth-service/internal/infra/db/migrate-entrypoint.sh
+++ auth-service/internal/infra/db/migrate-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "running create auth schema migration in public"
-migrate -path=/migrations \
-    -database="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable&search_path=public" \
-    up 1
+echo "Creating schema '${POSTGRES_AUTH_SCHEME}' if it does not exist..."
+PGPASSWORD="${POSTGRES_PASSWORD}" psql -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" \
+    -c "CREATE SCHEMA IF NOT EXISTS ${POSTGRES_AUTH_SCHEME};"
 
-echo "running all other migrations in auth schema"
-migrate -path=/migrations \
+echo "running migrations in '${POSTGRES_AUTH_SCHEME}' schema"
+migrate -path=/app/migrations \
     -database="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable&search_path=${POSTGRES_AUTH_SCHEME}" \
     up
