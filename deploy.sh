e048e1d4a6ca259a7cd0793ed31e1204dc9bda45
M0s1ck <senyamoskin@gmail.com>
2025-11-18 03:17:17 +0300

    add explicit docker compose in script

--- deploy.sh
+++ deploy.sh
@@ -12,17 +12,19 @@ source ./env
 # login to docker hub
 echo "${DOCKERHUB_TOKEN}" | docker login -u "${DOCKERHUB_USERNAME}" --password-stdin
 
-sudo docker compose -f docker-compose.prod.yaml pull
+# pull images
+docker compose -f docker-compose.prod.yaml pull
 
-sudo docker compose up psg -d
-
+# bring up db
+docker compose -f docker-compose.prod.yaml up psg -d
 sleep 10
 
-sudo docker compose run --rm auth-migrate
-sudo docker compose run --rm user-migrate
+# apply migrations
+docker compose -f docker-compose.prod.yaml run --rm auth-migrate
+docker compose -f docker-compose.prod.yaml run --rm user-migrate
 
-sudo docker compose -f docker-compose.prod.yaml up -d
-
+# bring up services
+docker compose -f docker-compose.prod.yaml up -d
 sleep 10
 
-sudo docker compose logs --tail=200
+docker compose logs --tail=200
